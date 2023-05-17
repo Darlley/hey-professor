3cdf5215c487ec209d8dd9389686621ca98577f5
Darlley Brito <darlleybrito@gmail.com>
2023-05-17 09:16:11 -0300

    HEY-11: should list all the questions

--- resources/views/components/question/list.blade.php
+++ resources/views/components/question/list.blade.php
@@ -1,16 +1,14 @@
 @props([
     'questions'
 ])
-<div class="p-4 text-gray-900 dark:text-gray-100 md:w-6/12 m-4 md:m-0 bg-white rounded-lg w-full box-content">
+<div class="p-4 text-gray-900 dark:text-gray-100 md:w-6/12 m-4 md:m-0 rounded-lg bg-white">
     <div class="mb-4">
         <h1 class="font-bold text-xl">{{ $slot }}</h1>
     </div>
-    <ul class="flex flex-col w-full box-content">
+    <ul class="flex flex-col w-full box-content bg-white">
         @foreach($questions as $key => $question)
         <li class="w-full flex flex-wrap">
-            <span class="w-full flex box-content break-all">
-                {{ $key +1 . ' - ' . $question->question }}
-            </span>
+            <span class="w-full break-all">{{ $key + 1 . ' - ' . $question->question }}</span>
         </li>
         @endforeach
     </ul>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex gap-4 w-full flex-col-reverse md:flex-row box-content">
+            <div class="flex gap-4 w-full flex-col-reverse md:flex-row box-border">
                 <x-question.list :questions="$questions">Todas as perguntas</x-question.list>
                 <x-question.container>Nova pergunta</x-question.container>
             </div>
